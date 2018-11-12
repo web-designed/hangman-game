47b1beee6c005392171bd706862361ec9509dd1c
kmianowski <krzysztof@creativeconstruction.de>
2018-11-12 14:55:46 +0100

    https vs. http

--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,7 @@
 //*******************************************************
 
    export const getPuzzle = async (wordcount) => {
-      const response = await fetch(`http://puzzle.mead.io/puzzle/?wordCount=${wordcount}`)
+      const response = await fetch(`//puzzle.mead.io/puzzle/?wordCount=${wordcount}`)
       if (response.status === 200) {
          const data = await response.json()
          return data.puzzle
